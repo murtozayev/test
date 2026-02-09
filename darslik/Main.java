4fdbff441d8255153c97e5107d0beb444bf47a94
murtozayev <jahongirmurtozayev777@gmail.com>
2026-02-09 06:38:17 -0500

    Test

--- darslik/Main.java
+++ darslik/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "hello World";
-        System.out.println(s.toUpperCase());
+        String s = "Hello World";
+        String trimmed = s.trim();
+        String[] strs = trimmed.split(" ");
+        Integer lastWord = strs[strs.length - 1].length();
     }
 }
